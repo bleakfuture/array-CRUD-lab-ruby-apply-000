ce3203f17032e946be844891ea33a5594abe2f16
Jasper De Luna <jasper.deluna@aol.com>
2018-09-29 10:42:25 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,11 +9,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- add_element_to_start_of_array << "array", "element"
+ add_element_to_end_of_array << "array", "element"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+element = add_element_to_start_of_array.shift
 end
 
 def remove_element_from_end_of_array(array)
