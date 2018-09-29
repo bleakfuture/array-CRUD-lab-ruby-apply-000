9bd884a1ed2971178969c8ecf3e7b497e946a46e
Jasper De Luna <jasper.deluna@aol.com>
2018-09-29 10:50:44 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,6 +14,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
+  array = ["Flynn", "Arlene", "Jasper", "Mom & Dad"]
 element = add_element_to_start_of_array.shift
 end
 
